ae1202e0a84f1aa790cbb58a7854f661ffb6f5c5
ersalinatrisna <ersalinatrisnawati@gmail.com>
2018-11-11 20:47:12 +0700

    update checkin 1

--- ManajemenParkir/src/CheckIn/CheckInUI.java
+++ ManajemenParkir/src/CheckIn/CheckInUI.java
@@ -7,7 +7,7 @@ package CheckIn;
 
 /**
  *
- * @author ASUS
+ * @author Ersalina
  */
 public class CheckInUI extends javax.swing.JFrame {
 
@@ -27,52 +27,124 @@ public class CheckInUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
+        panel1 = new java.awt.Panel();
+        CheckIn = new javax.swing.JLabel();
+        NomorKendaraan = new javax.swing.JLabel();
+        Lantai = new javax.swing.JLabel();
+        NomorLot = new javax.swing.JLabel();
+        TextNoKendaraan = new javax.swing.JTextField();
+        BoxLantai = new javax.swing.JComboBox<>();
+        BoxNomorLot = new javax.swing.JComboBox<>();
+        buttonCheckin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        jPanel2.setBackground(new java.awt.Color(102, 0, 255));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 188, Short.MAX_VALUE)
+        javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
+        panel1.setLayout(panel1Layout);
+        panel1Layout.setHorizontalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 145, Short.MAX_VALUE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 471, Short.MAX_VALUE)
+        panel1Layout.setVerticalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 518, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        CheckIn.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        CheckIn.setText("Check In Parking");
+
+        NomorKendaraan.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        NomorKendaraan.setText("Nomor Kendaraan :");
+
+        Lantai.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Lantai.setText("Lantai :");
+
+        NomorLot.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        NomorLot.setText("Nomor Lot :");
+
+        TextNoKendaraan.setMinimumSize(new java.awt.Dimension(5, 25));
+        TextNoKendaraan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TextNoKendaraanActionPerformed(evt);
+            }
+        });
+
+        BoxLantai.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Lantai 1", "Lantai 2", "Lantai 3" }));
+        BoxLantai.setMinimumSize(new java.awt.Dimension(5, 25));
+        BoxLantai.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxLantaiActionPerformed(evt);
+            }
+        });
+
+        BoxNomorLot.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Lot 1", "Lot 2", "Lot 3", "Lot 4", "Lot 5", "Lot 6", "Lot 7", "Lot 8", "Lot 9", "Lot 10" }));
+        BoxNomorLot.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxNomorLotActionPerformed(evt);
+            }
+        });
+
+        buttonCheckin.setText("CHECK IN");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(CheckIn)
+                        .addComponent(NomorKendaraan)
+                        .addComponent(NomorLot)
+                        .addComponent(TextNoKendaraan, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(BoxLantai, 0, 216, Short.MAX_VALUE)
+                        .addComponent(BoxNomorLot, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(Lantai))
+                .addGap(50, 50, 50)
+                .addComponent(buttonCheckin, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(44, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(CheckIn)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                .addComponent(NomorKendaraan)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(TextNoKendaraan, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(Lantai)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(BoxLantai, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(NomorLot))
+                    .addComponent(buttonCheckin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(BoxNomorLot, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(54, 54, 54))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void TextNoKendaraanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextNoKendaraanActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_TextNoKendaraanActionPerformed
+
+    private void BoxLantaiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxLantaiActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxLantaiActionPerformed
+
+    private void BoxNomorLotActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxNomorLotActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxNomorLotActionPerformed
 
     /**
      * @param args the command line arguments
@@ -110,7 +182,14 @@ public class CheckInUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JComboBox<String> BoxLantai;
+    private javax.swing.JComboBox<String> BoxNomorLot;
+    private javax.swing.JLabel CheckIn;
+    private javax.swing.JLabel Lantai;
+    private javax.swing.JLabel NomorKendaraan;
+    private javax.swing.JLabel NomorLot;
+    private javax.swing.JTextField TextNoKendaraan;
+    private javax.swing.JButton buttonCheckin;
+    private java.awt.Panel panel1;
     // End of variables declaration//GEN-END:variables
 }
